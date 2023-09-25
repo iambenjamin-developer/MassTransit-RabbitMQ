45528435b468c0aa736edcfd3a746b443d39d5a1
Benjamin Correa <iambenjamin.developer@gmail.com>
2023-09-25 09:43:22 -0300

    Agregar MassTransit y configurar en .net7

---
+++ WorkerService/Consumers/ProductMessageConsumer.cs
@@ -0,0 +1,28 @@
+﻿using Common.MassTransit.Models.Products;
+using MassTransit;
+
+namespace WorkerService.Consumers
+{
+    public class ProductMessageConsumer : IConsumer<IProductMessage>
+    {
+        private readonly ILogger<ProductMessageConsumer> _logger;
+
+        public ProductMessageConsumer(ILogger<ProductMessageConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<IProductMessage> context)
+        {
+
+            _logger.LogInformation($"Product: {context.Message.Name}");
+
+            _logger.LogInformation($"Precio anterior: ${context.Message.Price}");
+
+            _logger.LogInformation($"Precio actual(+15%): ${context.Message.Price * 1.15M}");
+
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- WorkerService/Program.cs
+++ WorkerService/Program.cs
@@ -1,10 +1,33 @@
-using WorkerService;
+using MassTransit;
+using WorkerService.Consumers;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
+try
+{
 
-host.Run();
+    var host = Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<ProductMessageConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(hostContext.Configuration["Rabbitmq:Url"]);
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+            services.AddMassTransitHostedService(true);
+        })
+        .Build();
+
+    await host.RunAsync();
+    return 0;
+}
+catch (Exception ex)
+{
+    return 1;
+}
+finally
+{
+}
